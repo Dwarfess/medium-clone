2ba4975d31886c013366c9fbf133b4138402e5e8
ssazo <ssazo@seftserveinc.com>
2022-08-02 15:16:37 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { BrowserRouter as Router } from 'react-router-dom'; 
+import Routes from 'routes';
+
+
+const App = () => {
+  return (
+    <div>
+      <h3>Welcome to hooks</h3>
+      <Router>
+        <Routes />
+      </Router>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
